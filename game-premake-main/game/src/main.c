039c1534ee2bdba627cd6eb7441eaaa60e1d85aa
Jacorey-creator <129239117+Jacorey-creator@users.noreply.github.com>
2024-05-20 19:24:13 -0600

    FixedTimeScale

--- game-premake-main/game/src/main.c
+++ game-premake-main/game/src/main.c
@@ -18,106 +18,111 @@ int main(void)
 {
 	nc_Body* selectedBody = NULL;
 	nc_Body* connectedBody = NULL;
+	nc_Contact_t* contacts = NULL;
+
+	float fixedTimestep = 1.0 / 60;
+	float timeAccumulator = 0;
 
 	InitWindow(1280, 720, "physics engine");
 	InitEditor();
-	SetTargetFPS(120);
+	SetTargetFPS(60);
 	//initialize world
-	//nc_Gravity = (Vector2){ 0 , -1 };
 
 	//game loop
 	while (!WindowShouldClose())
 	{
-		//Update
-		nc_Gravity = (Vector2){ 0 , -nc_EditorData.GravitationalValue };
+			//Update
+			nc_Gravity = (Vector2){ 0 , -nc_EditorData.GravitationalValue };
 
-		float dt = GetFrameTime();
-		float fps = (float)GetFPS();
-		Vector2 m_position = GetMousePosition();
-		ncScreenZoom += GetMouseWheelMove() * 0.2f;
-		ncScreenZoom = Clamp(ncScreenZoom, 0.1f, 10);
-		UpdateEditor(m_position);
+			float dt = GetFrameTime();
+			float fps = (float)GetFPS();
+			Vector2 m_position = GetMousePosition();
+			ncScreenZoom += GetMouseWheelMove() * 0.2f;
+			ncScreenZoom = Clamp(ncScreenZoom, 0.1f, 10);
+			UpdateEditor(m_position);
 
-		selectedBody = GetBodyIntersect(nc_Bodies, m_position);
-		if (selectedBody)
-		{
-			Vector2 screen = ConvertWorldToScreen(selectedBody->position);
-			DrawCircleLines((int)screen.x, (int)screen.y, ConvertWorldToPixel(selectedBody->mass) + 5, selectedBody->color);
-		}
-		if (!ncEditorIntersect)
-		{
-			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_LEFT) && IsKeyDown(KEY_LEFT_SHIFT))
+			selectedBody = GetBodyIntersect(nc_Bodies, m_position);
+			if (selectedBody)
 			{
-				nc_Body* body = CreateBody(ConvertScreenToWorld(m_position), nc_EditorData.MassValue, nc_EditorData.BodyType);
-
-				body->damping = nc_EditorData.DampenValue;
-				body->gravityScale = nc_EditorData.BodyGravityValue;
-				body->color = ColorFromHSV(GetRandomFloatValue02(0, 360), 1, 1);
-				body->restitution = 0.8f;
-				AddBody(body);
+				Vector2 screen = ConvertWorldToScreen(selectedBody->position);
+				DrawCircleLines((int)screen.x, (int)screen.y, ConvertWorldToPixel(selectedBody->mass) + 5, selectedBody->color);
 			}
-			//Connect Spring
-			if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && selectedBody) connectedBody = selectedBody;
-			if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && connectedBody) DrawLineBodyToPosition(connectedBody, m_position);
-			if (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT) && connectedBody)
+			if (!ncEditorIntersect)
 			{
-				if (selectedBody && selectedBody != connectedBody)
+				if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_LEFT) && IsKeyDown(KEY_LEFT_SHIFT))
 				{
-					nc_Spring_t* spring = CreateSpring(connectedBody, selectedBody, Vector2Distance(connectedBody->position, selectedBody->position),nc_EditorData.kValue);
-					AddSpring(spring);
+					nc_Body* body = CreateBody(ConvertScreenToWorld(m_position), nc_EditorData.MassValue, nc_EditorData.BodyType);
+
+					body->damping = nc_EditorData.DampenValue;
+					body->gravityScale = nc_EditorData.BodyGravityValue;
+					body->color = ColorFromHSV(GetRandomFloatValue02(0, 360), 1, 1);
+					body->restitution = 0.8f;
+					AddBody(body);
+				}
+				//Connect Spring
+				if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && selectedBody) connectedBody = selectedBody;
+				if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && connectedBody) DrawLineBodyToPosition(connectedBody, m_position);
+				if (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT) && connectedBody)
+				{
+					if (selectedBody && selectedBody != connectedBody)
+					{
+						nc_Spring_t* spring = CreateSpring(connectedBody, selectedBody, Vector2Distance(connectedBody->position, selectedBody->position), nc_EditorData.kValue);
+						AddSpring(spring);
+					}
 				}
 			}
+
+			while (timeAccumulator >= fixedTimestep)
+			{
+				timeAccumulator = timeAccumulator - fixedTimestep;
+			}
+			//apply force
+
+			ApplyGravitation(nc_Bodies, nc_EditorData.GravitationalValue);
+			ApplySpringForce(nc_Springs);
+			timeAccumulator += fixedTimestep;
+			//update bodies
+			for (nc_Body* body = nc_Bodies; body != NULL; body = body->next)
+			{
+				Step(body, fixedTimestep);
+			}
+			CreateContacts(nc_Bodies, &contacts);
+			SeparateContacts(contacts);
+			ResolveContacts(contacts);
+			//DestroyAllContacts(contacts);
+			//Render
+			BeginDrawing();
+			ClearBackground(BLACK);
+
+			//stats
+			DrawText(TextFormat("FPS: %.2f (ms %.2fms)", fps, 1000 / fps), 10, 10, 20, LIME);
+			DrawText(TextFormat("FRAME: %.4f", dt), 10, 30, 20, LIME);
+
+			// draw bodies
+			for (nc_Body* body = nc_Bodies; body; body = body->next)
+			{
+				Vector2 screen = ConvertWorldToScreen(body->position);
+				DrawCircle((int)screen.x, (int)screen.y, ConvertWorldToPixel(body->mass * 0.5f), body->color);
+			}
+
+			// draw contacts
+			for (nc_Contact_t* contact = contacts; contact; contact = contact->next)
+			{
+				Vector2 screen = ConvertWorldToScreen(contact->body1->position);
+				DrawCircle((int)screen.x, (int)screen.y, ConvertWorldToPixel(contact->body1->mass * 0.5f), RED);
+			}
+
+			//draw springs
+			for (nc_Spring_t* spring = nc_Springs; spring; spring = spring->next)
+			{
+				Vector2 screen1 = ConvertWorldToScreen(spring->body1->position);
+				Vector2 screen2 = ConvertWorldToScreen(spring->body2->position);
+				DrawLine((int)screen1.x, (int)screen1.y, (int)screen2.x, (int)screen2.y, YELLOW);
+			}
+			DrawEditor(m_position);
+
+			EndDrawing();
 		}
-		//apply force
-
-		ApplyGravitation(nc_Bodies, nc_EditorData.GravitationalValue);
-		ApplySpringForce(nc_Springs);
-
-		//update bodies
-		for (nc_Body* body = nc_Bodies; body != NULL; body = body->next)
-		{
-			Step(body, dt);
-		}
-
-		//Collision
-		nc_Contact_t* contacts = NULL;
-		CreateContacts(nc_Bodies, &contacts);
-		SeparateContacts(contacts);
-		//ResolveContacts(contacts);
-
-		//Render
-		BeginDrawing();
-		ClearBackground(BLACK);
-
-		//stats
-		DrawText(TextFormat("FPS: %.2f (ms %.2fms)", fps, 1000 / fps), 10, 10, 20, LIME);
-		DrawText(TextFormat("FRAME: %.4f", dt), 10, 30, 20, LIME);
-
-		// draw bodies
-		for (nc_Body* body = nc_Bodies; body; body = body->next)
-		{
-			Vector2 screen = ConvertWorldToScreen(body->position);
-			DrawCircle((int)screen.x, (int)screen.y, ConvertWorldToPixel(body->mass * 0.5f), body->color);
-		}
-
-		// draw contacts
-		for (nc_Contact_t* contact = contacts; contact; contact = contact->next)
-		{
-			Vector2 screen = ConvertWorldToScreen(contact->body1->position);
-			DrawCircle((int)screen.x, (int)screen.y, ConvertWorldToPixel(contact->body1->mass * 0.5f), RED);
-		}
-
-		//draw springs
-		for (nc_Spring_t* spring = nc_Springs; spring; spring = spring->next)
-		{
-			Vector2 screen1 = ConvertWorldToScreen(spring->body1->position);
-			Vector2 screen2 = ConvertWorldToScreen(spring->body2->position);
-			DrawLine((int)screen1.x, (int)screen1.y, (int)screen2.x, (int)screen2.y, YELLOW);
-		}
-		DrawEditor(m_position);
-		
-		EndDrawing();
-	}
 	CloseWindow();
 	free(nc_Bodies);
 
